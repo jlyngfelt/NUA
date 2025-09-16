a9c08e2bce2eb11d291cf263126dda96a8a0c22e
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-09-16 14:53:52 +0200

    created first struckture

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,31 @@
+import Link from 'next/link'
 import Hoodie from '../components/Hoodie'
 
 export default function Home() {
   return (
     <div>
-      <h1>hej</h1>
-      <Hoodie />
+        <nav>
+        {/* Din navbar här */}
+      </nav>
+         <div>
+        {/* Första bilden med knapp som ska vara död */}
+        <div>
+          <img src="/bild1.jpg" alt="Bild 1" />
+          <button disabled>Kommer snart</button>
+        </div>
+        
+        {/* Andra bilden med knapp som leder vidare */}
+        <div>
+          <img src="/bild2.jpg" alt="Bild 2" />
+          <Link href="/product-selection">
+            <button>Gå vidare</button>
+          </Link>
+        </div>
+      </div>
+       <footer>
+        {/* Din footer här */}
+      </footer>
+      
     </div>
   )
 }
