8044400753ad680a046ab7be86dcc3d10b8965f7
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-09-16 14:53:52 +0200

    created first struckture

---
+++ pages/products.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export default function ProductSelection() {
+  return (
+    <div>
+       <nav>
+        {/* Din navbar här */}
+      </nav>
+      
+      <h1>Här gör du dina val</h1>
+      
+        <footer>
+        {/* Din footer här */}
+      </footer>
+    </div>
+  )
+}
