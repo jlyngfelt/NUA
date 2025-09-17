5a6f1f792744b6e202ec4c1b2271d92fa7f04841
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-09-17 10:08:58 +0200

    first draft of size and fit

---
+++ components/FitAndSize/FitAndSize.js
@@ -0,0 +1,79 @@
+import Image from "next/image";
+import styles from "./FitAndSize.module.css";
+import {useState} from 'react';
+
+export const FitAndSize = () => {
+    const [activeTab, setActiveTab] = useState(0);
+
+    return (
+<div>
+<div className={styles.sizeContainer}>
+    <h3>Size:</h3>
+        <button
+          className={`${styles.tabMenuButton} ${
+            activeTab === 0 ? styles.active : ""
+          }`}
+          onClick={() => setActiveTab(0)}
+        >
+          xs
+        </button>
+        <button
+          className={`${styles.tabMenuButton} ${
+            activeTab === 1 ? styles.active : ""
+          }`}
+          onClick={() => setActiveTab(1)}
+        >
+          s
+        </button>
+        <button
+          className={`${styles.tabMenuButton} ${
+            activeTab === 2 ? styles.active : ""
+          }`}
+          onClick={() => setActiveTab(2)}
+        >
+          m
+        </button>
+        <button
+          className={`${styles.tabMenuButton} ${
+            activeTab === 3 ? styles.active : ""
+          }`}
+          onClick={() => setActiveTab(3)}
+        >
+          l
+        </button>
+        <button
+          className={`${styles.tabMenuButton} ${
+            activeTab === 4 ? styles.active : ""
+          }`}
+          onClick={() => setActiveTab(4)}
+        >
+          xl
+        </button>
+        <button
+          className={`${styles.tabMenuButton} ${
+            activeTab === 5 ? styles.active : ""
+          }`}
+          onClick={() => setActiveTab(5)}
+        >
+          xxl
+        </button>
+        <button
+          className={`${styles.tabMenuButton} ${
+            activeTab === 6 ? styles.active : ""
+          }`}
+          onClick={() => setActiveTab(6)}
+        >
+          3xl
+        </button>
+</div>
+
+<div className={styles.fitContainer}>
+
+    <button>
+        Continue
+    </button>
+</div>
+
+</div>
+    )
+}
