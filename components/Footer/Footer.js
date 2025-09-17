26efe56b1ba2159aaae95c2b20ed061e54d262d0
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-09-17 08:25:02 +0200

    footer created

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,16 +1,37 @@
-import styles from './Footer.module.css'
+import Image from "next/image";
+import styles from "./Footer.module.css";
 
 export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.buttonLogoGroup}>
+        <Image src="/icons/LogoLarge.png" alt="Logo" width={177} height={55} />
 
-    return (
-        <footer className={styles.footer}>
-            <div className={styles.menuContainer}>
+        <div className={styles.menuContainer}>
+          <div className={styles.buttonGroup}>
+            <button className={styles.button}>Q&A</button>
+            <button className={styles.button}>Contact</button>
+            <button className={styles.button}>Support</button>
+          </div>
 
-            </div>
+          <div className={styles.divider}></div>
 
-            <div className={styles.socialMedia}>
-
-            </div>
-        </footer>
-)
-}
+          <div className={styles.buttonGroup}>
+            <button className={styles.button}>Delivery</button>
+            <button className={styles.button}>About us</button>
+            <button className={styles.button}>Return Policy</button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.socialMedia}>
+        <h2>Follow us</h2>
+        <div className={styles.iconContainer}>
+        <Image src="/icons/IconFb.png" alt="Logo" width={48} height={48} className={styles.icon} />
+        <Image src="/icons/IconInsta.png" alt="Logo" width={48} height={48} className={styles.icon}/>
+        <Image src="/icons/IconLinkedIn.png" alt="Logo" width={48} height={48} className={styles.icon} />
+        <Image src="/icons/IconTikTok.png" alt="Logo" width={48} height={48} className={styles.icon}/>
+        </div>
+      </div>
+    </footer>
+  );
+};
