5744475a938fb5a6c49feed02fa3a29c7dbcec6f
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-09-17 09:01:56 +0200

    creatded configuratorcard and css file for it

---
+++ components/ComfiguratorCard/ConfiguratorCard.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./ConfiguratorCard.module.css";
+
+export const ConfiguratorCard = () => {
+
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
