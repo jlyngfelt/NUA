c5f29d2bb7890b46b258a82b5ae4ba29a4d64bb6
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-09-18 08:11:17 +0200

    added placeholderimages for landingpage and buttonstyling

--- components/Landingpage/Landingpage.js
+++ components/Landingpage/Landingpage.js
@@ -1,27 +1,36 @@
-      
-      
-      
-      
-      <div className={styles.mainContent}>
-        <div>
-          <Image
-            src="/path/to/ImageBrowse.png"
-            alt="Hoodie"
-            fill
-            style={{ objectFit: "cover" }} 
-          />
-          <button disabled>Browse collection</button>
-        </div>
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Landingpage.module.css";
+import { useState } from "react";
 
-        <div>
-           <Image
-            src="/path/to/ImageBrowse.png"
-            alt="Hoodie"
-            fill
-            style={{ objectFit: "cover" }} 
-          />
-          <Link href="/products">
-            <button>Design your own</button>
-          </Link>
-        </div>
+export const Landingpage = () => {
+  return (
+    <div className={styles.mainContent}>
+      <div className={styles.imageContainer}>
+        <Image
+          src="/images/Landing_page_Image__button_left.png"
+          alt="Hoodie"
+          width={0}
+          height={0}
+          sizes="50vw"
+          style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        />
+        <button className={styles.button}>Browse collection</button>
       </div>
+
+      <div className={styles.imageContainer}>
+         <Image
+          src="/images/Landing_page_Image__button_right.png"
+          alt="Hoodie"
+          width={0}
+          height={0}
+          sizes="50vw"
+          style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        />
+        <Link href="/products">
+          <button className={styles.button}>Design your own</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
