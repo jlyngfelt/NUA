992316040889db1dd25fb22124342e562e070430
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-09-18 10:12:35 +0200

    added logic for changing tabs and on c button

--- components/button/continueButton/ContinueButton.js
+++ components/button/continueButton/ContinueButton.js
@@ -2,12 +2,12 @@ import Image from "next/image";
 import styles from "./ContinueButton.module.css";
 import {useState} from 'react';
 
-export const ContinueButton = ({children}) => {
+export const ContinueButton = ({children, onClick}) => {
     const [activeTab, setActiveTab] = useState(0);
 
     return (
         <>
-<button className={styles.button}>{children}</button>
+<button className={styles.button} onClick={onClick}>{children}</button>
         </>
     )
 }
