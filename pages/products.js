b1083f0bfaca4d25243b4e999d1226a82393fa85
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-09-18 12:52:52 +0200

    Added and styled navbanner

--- pages/products.js
+++ pages/products.js
@@ -6,6 +6,7 @@ import { Hoodie } from "../components/Hoodie";
 import { ConfiguratorCard } from "../components/ComfiguratorCard/ConfiguratorCard";
 import { Description } from "../components/Description/Description";
 import { defaultColors } from "../components/Hoodie/config/colorConfig";
+import { NavigationBanner } from "../components/NavigationBanner/NavigationBanner";
 
 export default function ProductSelection() {
     const [customColors, setCustomColors] = useState(defaultColors);
@@ -21,7 +22,7 @@ export default function ProductSelection() {
     <div>
       <Menu />
 
-      <h1>produktsida</h1>
+      <NavigationBanner/>
         <main >
       <Hoodie customColors={customColors} onColorChange={handleColorChange}/>
       <ConfiguratorCard customColors={customColors} onColorChange={handleColorChange} />
