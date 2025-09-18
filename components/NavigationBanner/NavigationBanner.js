8b53b94710624afc1635340bcb4c2777e951df15
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-09-18 12:52:52 +0200

    Added and styled navbanner

---
+++ components/NavigationBanner/NavigationBanner.js
@@ -0,0 +1,13 @@
+
+import styles from "./NavigationBanner.module.css";
+
+
+export const NavigationBanner = () => {
+   
+
+    return (
+        <>
+<h2 className={styles.navigation}>Mens/Tops/Hoodies/<span className={styles.span}>Custom design</span></h2>
+        </>
+    )
+}
