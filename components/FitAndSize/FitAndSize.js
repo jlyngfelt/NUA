5ed14ac9b63609a56d7c04ae50dbc6a6871d02f1
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-09-22 11:06:53 +0200

    added styling for losefit/tightfit

--- components/FitAndSize/FitAndSize.js
+++ components/FitAndSize/FitAndSize.js
@@ -1,81 +1,111 @@
 import Image from "next/image";
 import styles from "./FitAndSize.module.css";
-import {useState} from 'react';
-import {ContinueButton} from '../button/continueButton/ContinueButton';
+import { useState } from "react";
+import { ContinueButton } from "../button/continueButton/ContinueButton";
 
-export const FitAndSize = ({handleOnClick}) => {
-    const [activeTab, setActiveTab] = useState(0);
+export const FitAndSize = ({ handleOnClick }) => {
+  const [activeTab, setActiveTab] = useState(0);
+  const [activeFit, setActiveFit] = useState(0);
 
-    return (
-<div>
-<div className={styles.sizeContainer}>
-    <h3>Size:</h3>
 
-<div className={styles.sizes}>
-        <button
-          className={`${styles.tabMenuButton} ${
-            activeTab === 0 ? styles.active : ""
-          }`}
-          onClick={() => setActiveTab(0)}
-        >
-          xs
-        </button>
-        <button
-          className={`${styles.tabMenuButton} ${
-            activeTab === 1 ? styles.active : ""
-          }`}
-          onClick={() => setActiveTab(1)}
-        >
-          s
-        </button>
-        <button
-          className={`${styles.tabMenuButton} ${
-            activeTab === 2 ? styles.active : ""
-          }`}
-          onClick={() => setActiveTab(2)}
-        >
-          m
-        </button>
-        <button
-          className={`${styles.tabMenuButton} ${
-            activeTab === 3 ? styles.active : ""
-          }`}
-          onClick={() => setActiveTab(3)}
-        >
-          l
-        </button>
-        <button
-          className={`${styles.tabMenuButton} ${
-            activeTab === 4 ? styles.active : ""
-          }`}
-          onClick={() => setActiveTab(4)}
-        >
-          xl
-        </button>
-        <button
-          className={`${styles.tabMenuButton} ${
-            activeTab === 5 ? styles.active : ""
-          }`}
-          onClick={() => setActiveTab(5)}
-        >
-          xxl
-        </button>
-        <button
-          className={`${styles.tabMenuButton} ${
-            activeTab === 6 ? styles.active : ""
-          }`}
-          onClick={() => setActiveTab(6)}
-        >
-          3xl
-        </button>
+  return (
+    <div className={styles.fitAndSize}>
+      <div className={styles.sizeContainer}>
+        <h3>Size:</h3>
+
+        <div className={styles.sizes}>
+          <button
+            className={`${styles.tabMenuButton} ${
+              activeTab === 0 ? styles.active : ""
+            }`}
+            onClick={() => setActiveTab(0)}
+          >
+            xs
+          </button>
+          <button
+            className={`${styles.tabMenuButton} ${
+              activeTab === 1 ? styles.active : ""
+            }`}
+            onClick={() => setActiveTab(1)}
+          >
+            s
+          </button>
+          <button
+            className={`${styles.tabMenuButton} ${
+              activeTab === 2 ? styles.active : ""
+            }`}
+            onClick={() => setActiveTab(2)}
+          >
+            m
+          </button>
+          <button
+            className={`${styles.tabMenuButton} ${
+              activeTab === 3 ? styles.active : ""
+            }`}
+            onClick={() => setActiveTab(3)}
+          >
+            l
+          </button>
+          <button
+            className={`${styles.tabMenuButton} ${
+              activeTab === 4 ? styles.active : ""
+            }`}
+            onClick={() => setActiveTab(4)}
+          >
+            xl
+          </button>
+          <button
+            className={`${styles.tabMenuButton} ${
+              activeTab === 5 ? styles.active : ""
+            }`}
+            onClick={() => setActiveTab(5)}
+          >
+            xxl
+          </button>
+          <button
+            className={`${styles.tabMenuButton} ${
+              activeTab === 6 ? styles.active : ""
+            }`}
+            onClick={() => setActiveTab(6)}
+          >
+            3xl
+          </button>
         </div>
+      </div>
+
+        <h3>Fit:</h3>
+      <div className={styles.imageContainer}>
+  <div 
+    className={`${styles.fitWrapper} ${activeFit === 1 ? styles.activeFit : ''}`}
+    onClick={() => setActiveFit(1)}
+  >
+    <Image
+      src="/images/LooseFit.png"
+      alt="Tight Fit Hoodie"
+      width={0}
+      height={0}
+      sizes="50vw"
+      className={styles.fit}
+    />
+  </div>
+
+  <div 
+    className={`${styles.fitWrapper} ${activeFit === 2 ? styles.activeFit : ''}`}
+    onClick={() => setActiveFit(2)}
+  >
+    <Image
+      src="/images/LooseFit.png"
+      alt="Loose Fit Hoodie"
+      width={0}
+      height={0}
+      sizes="50vw"
+      className={styles.fit}
+    />
+  </div>
 </div>
 
-<div className={styles.fitContainer}>
-
-</div>
-<ContinueButton onClick={handleOnClick}>Continue</ContinueButton>
-
-</div>
-    )
-}
+      <div className={styles.fitContainer}></div>
+      <ContinueButton onClick={handleOnClick}>Continue</ContinueButton>
+    </div>
+  );
+};
