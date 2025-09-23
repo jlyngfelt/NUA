db6c52fa8efd8602fdd698731de924f426dddd52
Mahtte94 <mahtias.jebrand@live.se>
2025-09-23 21:52:32 +0200

    removed console.logs and changed default color

--- pages/products.js
+++ pages/products.js
@@ -7,9 +7,16 @@ import { ConfiguratorCard } from "../components/ComfiguratorCard/ConfiguratorCar
 import { Description } from "../components/Description/Description";
 import { NavigationBanner } from "../components/NavigationBanner/NavigationBanner";
 import { defaultMaterialSelections, calculateMaterialCost } from "../components/Hoodie/config/materialConfig";
+import { colorOptions } from "../components/Hoodie/config/colorConfig";
+
+const defaultColors = {
+  body: colorOptions.body[0].color,
+  hoodInterior: colorOptions.hoodInterior[0].color,
+  zipperDetails: colorOptions.zipperDetails[0].color
+};
 
 export default function ProductSelection() {
-    const [customColors, setCustomColors] = useState({});
+    const [customColors, setCustomColors] = useState(defaultColors);
     const [materialSelections, setMaterialSelections] = useState(defaultMaterialSelections);
 
     const handleColorChange = (part, color) => {
@@ -27,7 +34,7 @@ export default function ProductSelection() {
     };
 
     const handleReset = () => {
-      setCustomColors({});
+      setCustomColors(defaultColors);
       setMaterialSelections(defaultMaterialSelections);
     };
 
