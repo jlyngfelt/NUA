42ccf9e34d9d3851c03fa71e106895a3487ab79e
Mahtte94 <mahtias.jebrand@live.se>
2025-09-25 14:19:55 +0200

    fixed styling on buttons and added fake switch button

--- components/Hoodie/Hoodie.js
+++ components/Hoodie/Hoodie.js
@@ -1,24 +1,24 @@
 import { useRef } from "react";
 import styles from "./Hoodie.module.css";
-import { CameraControls } from './components/CameraControls';
-import { useHoodieModel } from './hooks/useHoodieModel';
+import { CameraControls } from "./components/CameraControls";
+import { useHoodieModel } from "./hooks/useHoodieModel";
+import { SwitchButton } from "../button/switchButton/SwitchButton";
 
 export const Hoodie = ({ customColors, onColorChange, materialSelections }) => {
   const mountRef = useRef(null);
 
   // Initialize 3D model and get control functions
-  const { setCameraView, handleZoom } = useHoodieModel(mountRef, customColors, materialSelections);
-
-
+  const { setCameraView, handleZoom } = useHoodieModel(
+    mountRef,
+    customColors,
+    materialSelections
+  );
 
   return (
     <div className={styles.container}>
       <div ref={mountRef} />
-
-      <CameraControls
-        onViewChange={setCameraView}
-        onZoom={handleZoom}
-      />
+      <SwitchButton />
+      <CameraControls onViewChange={setCameraView} onZoom={handleZoom} />
     </div>
   );
 };
