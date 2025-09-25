22f4f924c625053e5c9669eff8ec42b3aeeb4d14
Mahtte94 <mahtias.jebrand@live.se>
2025-09-25 12:20:09 +0200

    resolved conflicts

--- components/FitAndSize/FitAndSize.js
+++ components/FitAndSize/FitAndSize.js
@@ -107,13 +107,8 @@ export const FitAndSize = ({ handleOnClick }) => {
   </div>
 </div>
 
-<<<<<<< HEAD
       <div className={styles.fitContainer}></div>
       <ContinueButton onClick={handleOnClick}>Continue</ContinueButton>
-=======
-      {/* <div className={styles.fitContainer}></div>
-      <ContinueButton onClick={handleOnClick}>Continue</ContinueButton> */}
->>>>>>> dev
     </div>
   );
 };
